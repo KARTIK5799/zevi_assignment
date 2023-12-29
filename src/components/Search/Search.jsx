96e8f25f231161bb03300c005fff535008a9df35
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-29 13:33:13 +0530

    Search component

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,31 @@
+
+import SearchIcon from "../../assets/search.png";
+import styles from "./Search.module.css";
+
+const Search = ({ onClick, onBlur, productStyle }) => {
+  return (
+    <form
+      action="submit"
+      className={productStyle ? styles.productForm : styles.searchForm}
+    >
+      <div
+        className={
+          productStyle ? styles.productFormContent : styles.searchFormContent
+        }
+      >
+        <input
+          type="text"
+          placeholder="Search"
+          className={styles.inputField}
+          onClick={onClick}
+          onBlur={onBlur}
+        />
+        <button type="submit" className={styles.searchButton}>
+          <img src={SearchIcon} alt="Search" />
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Search;
